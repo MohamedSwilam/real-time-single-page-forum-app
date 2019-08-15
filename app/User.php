51f68559aaaa345eea73d1b660bdf3550f0b39ef
Mohamed Ehab Swilam <mohamed_swilam@hotmail.com>
2019-08-15 18:25:25 +0200

    JWT Package

--- app/User.php
+++ app/User.php
@@ -8,8 +8,9 @@ use App\Model\Reply;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -53,5 +54,25 @@ class User extends Authenticatable
     public function like()
     {
         $this->hasMany(Likes::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
